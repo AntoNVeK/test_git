49c1005ec54f7adb1a7319913d4c6d51236334eb
Корниенко Антон <111607633+AntoNVeK@users.noreply.github.com>
2024-02-20 22:18:45 +0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,159 +1,92 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
-#include <ctype.h>
-#include <assert.h>
-
-
-
-
-int count(char* str){
-
-    int count_space = 0;
-
-    int flag = 1;
-
-    for(int i = 0;i < strlen(str);i++){
-
-        if(str[i] == ' ' && flag){
-
-            count_space ++;
-
-            flag = 0;
-
-        }
-
-        else if(str[i] != ' ')
-
-            flag = 1;
-
-    }
-
-    char** text = malloc((count_space + 1) * sizeof(char*));
-
-    
-
-
-
-    char* word = NULL;
-
-    int idx = 0;
-
-    int number = 0;
-
-    int count_s = 0;
-
-    for(int i = 0; i < strlen(str);i++){
-
-        
-
-        if(str[i] != ' '){
-
-            word = realloc(word, sizeof(char) * (++idx));
-
-            word[idx - 1] = str[i];
-            count_s = 0;
-            
-
-        }
-
-
-
-        if((str[i] == ' ' && i > 0 && count_s == 0) || i == strlen(str) - 1){
-
-            word = realloc(word, sizeof(char) * ((++idx) ));
-
- 
-
-            word[idx - 1] = '\0';
-            count_s = 1;
-
-
-            
-
-
-
-            text[number] = malloc((strlen(word) + 1) * sizeof(char));
-
-            for(int k = 0; k < strlen(word); k++){
-
-                text[number][k] = word[k];
-
-            }   
-
-             text[number][strlen(word)] = '\0';
-
-             number++;
-
-             
-
-             
-
-             idx = 0;           
-
-        }
-
-
-
-    }
-
-
-
-    int Max = 1;
-
-    for(int j = 0; j < number - 1; j++){
-
-        int tek_count = 1;
-
-        for(int k = j + 1; k < number; k++){
-
-            if(strcmp(text[j],text[k]) == 0){
-
-                tek_count++;
-
-                if(tek_count > Max)
-
-                    Max = tek_count;
-
-
-
+#include<ctype.h>
+void printFile(char* filename, char* name_command){
+    int res = 0;
+
+   // if(strcmp(name_command, "mul") == 0)
+   //     res = 1;
+
+    FILE *f = fopen(filename, "r");
+
+    if(!f)
+        return;
+    printf("filename : %s:\n", filename);
+    char s[100];
+    while(fgets(s,100,f)){
+        printf("\t%s: ",s);
+        char num[10];
+        int size = 0;
+        for(int i = 0; i < strlen(s); i ++){
+            if(isdigit(s[i]) || s[i] == '-'){
+                num[size] = s[i];
+                size++;
             }
-
+            if(s[i] == ' ' || i == strlen(s) - 1){
+                num[size] = '\0';
+                if(strcmp(name_command, "add") == 0){
+                    res += atoi(num);
+                }
+                else if(strcmp(name_command, "mul") == 0){
+                    res *= atoi(num);
+                }
+                
+                size = 0;
+            }
         }
-
-
-
     }
-    free(word);
-    for(int i = 0; i < number;i ++){
-        free(text[i]);
+    fclose(f);
+    printf("%d\n", res);
+
+
+}
+void listDir(char* startDir, char* name_command){
+    char next[200] = {0};
+    strcpy(next, startDir);
+    DIR *dir = opendir(startDir);
+    if(!dir)
+        return;
+
+    struct dirent *de = readdir(dir);
+
+    while(de){
+        if(de->d_type == DT_DIR && strcmp(de->d_name, ".")
+         && strcmp(de->d_name, ".."))
+            strcpy(name_command,de->d_name);
+            name_command[4] = '\0';
+
+        if(de->d_type == DT_REG)
+            printf("de->d_name: %s\n", de->d_name);
+        if(de->d_type == DT_DIR && strcmp(de->d_name, ".")
+         && strcmp(de->d_name, ".."))
+        {
+            int len = strlen(next);
+            strcat(next, "/");
+            strcat(next, de->d_name);
+            listDir(next, name_command);
+            next[len] = '\0';  
+        }
+        if(de->d_type == DT_REG){
+            int len = strlen(next);
+            strcat(next, "/");
+            strcat(next, de->d_name);
+            printFile(next, name_command);
+            next[len] = '\0';  
+        }
+        de = readdir(dir);
     }
-    free(text);
-
-
-    return Max;
+    closedir(dir);
 
 }
 
 
 
-
-
 int main(){
+    char* name_command = malloc(4 * sizeof(char));
 
-    char s[201];
-
-    fgets(s, 201, stdin);
-
-    s[strcspn(s, "\n")] = 0;
-
-
-
-    int c = count(s);
-
-    printf("%d", c);
+    listDir("..\\..\\root", name_command);
 
     return 0;
-
 }
